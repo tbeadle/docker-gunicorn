929c61cc1d0fa4d2db02169bf6688a024a931618
Tommy Beadle <tbeadle@gmail.com>
2016-12-20 11:58:41 -0500

    Add some standard tasks for a Django app.
    If we detect that the application is Django-based, then do some basic
    things that will want to be done pretty much every time:
    
     - Collect static files to the location that nginx will serve up.
     - Wait for the database to be available and then perform database
       migrations.
     - Install a cron job to run './manage.py clearsessions' daily.

--- deploy/run.py
+++ deploy/run.py
@@ -4,6 +4,14 @@ from __future__ import print_function
 
 import jinja2
 import os
+import subprocess
+import time
+
+__metaclass__ = type
+
+class InvalidProjectError(Exception):
+    pass
+
 
 NGINX_VARS = {
     'CA_CERT': os.environ.get('CA_CERT', ''),
@@ -40,7 +48,7 @@ def render_template(name, tpl_vars, outfile):
     with open(outfile, 'w') as fil:
         print(template.render(**tpl_vars), file=fil)
 
-def main():
+def render_templates():
     render_template(
         'nginx.conf.jinja',
         NGINX_VARS,
@@ -56,6 +64,94 @@ def main():
         GUNICORN_VARS,
         '/etc/gunicorn.conf'
     )
+
+
+class DjangoHook:
+    def __init__(self):
+        try:
+            import django
+        except ImportError:
+            raise InvalidProjectError
+        self.manage_path = os.path.join(os.environ['APP_ROOT'], 'manage.py')
+        if not os.path.exists(self.manage_path):
+            raise InvalidProjectError
+        try:
+            if 'django' not in subprocess.check_output(
+                    self.manage_path).decode('utf-8', errors='ignore'):
+                raise InvalidProjectError
+        except subprocess.CalledProcessError:
+            raise InvalidProjectError
+        print('Detected a django project...')
+
+    def add_clearsessions_cron_job(self):
+        print('Adding clearsessions daily cron job.')
+        path = '/etc/cron.daily/clearsessions'
+        with open(path, 'w') as fil:
+            print(
+                "#!/bin/bash\n"
+                "{} clearsessions >/dev/null 2>&1\n".format(self.manage_path),
+                file=fil
+            )
+        os.chmod(path, 0o755)
+
+    def collectstatic(self):
+        print('Collecting static files.')
+        subprocess.check_call([
+            self.manage_path,
+            'collectstatic',
+            '-l',
+            '--noinput'
+        ])
+
+    def migrate_db(self):
+        has_db = bool(
+            subprocess.check_output([
+                self.manage_path,
+                'shell',
+                '-c',
+                'from django.conf import settings; '
+                'print(settings.DATABASES or "")'
+            ]).strip()
+        )
+        if not has_db:
+            return
+
+        print('Waiting for database to be ready.')
+        for _ in range(20):
+            output = subprocess.check_output([
+                self.manage_path,
+                'shell',
+                '-c',
+                'from django.db.utils import OperationalError\n'
+                'from django.db import connection\n'
+                'try:\n'
+                '    connection.cursor()\n'
+                'except OperationalError:\n'
+                '    print("Database not ready yet")'
+            ])
+            if not output:
+                break
+            else:
+                time.sleep(1)
+        else:
+            raise RuntimeError('Database is not ready to accept connections.')
+
+        print('Running migrations on database.')
+        subprocess.check_call([self.manage_path, 'migrate'])
+
+    def process(self):
+        self.add_clearsessions_cron_job()
+        self.migrate_db()
+        self.collectstatic()
+
+def main():
+    render_templates()
+    try:
+        proj = DjangoHook()
+    except InvalidProjectError:
+        pass
+    else:
+        proj.process()
 
 if __name__ == '__main__':
     main()
